bed72ed606394926b212f059ff69602e901fc65a
Ryan Womack <ryan@ryanwomack.com>
2026-09-09 15:35:47 +0000

    R file updated

--- data_topics/data_analysis_1/data_analysis_1.R
+++ data_topics/data_analysis_1/data_analysis_1.R
@@ -1,7 +1,7 @@
 # Data Analysis 1
 # Ryan Womack, ryan@ryanwomack.com
 # 2026-09-09 version
-# Copyright Ryan Womack, 2025. This work is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+# Copyright Ryan Womack, 2026. This work is licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 
 
 ## ----setup, include=FALSE-----------------------------------------------------
